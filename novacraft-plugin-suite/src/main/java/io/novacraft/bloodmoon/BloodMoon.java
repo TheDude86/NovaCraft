2dc7ba71dd5ed74ac63d86aae18aec50b4da1351
Aru Gyani <aru@gyani.com>
2019-07-30 17:36:46 -0500

    First commit of BloodMoon protoype.
    Sets up access to current moon phase. Includes enum for consistency sake.

---
+++ novacraft-plugin-suite/src/main/java/io/novacraft/bloodmoon/BloodMoon.java
@@ -0,0 +1,41 @@
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BloodMoon extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("BloodMoon has been enabled.");
+		PluginManager pm = getServer().getPluginManager();
+		 BloodMoonListener listener = new BloodMoonListener(this);
+		 pm.registerEvents(listener, this);
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("BloodMoon has been disabled.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		World world = player.getWorld();
+		
+		if(sender instanceof Player) {
+			String lowerCmd = cmd.getName().toLowerCase();
+			switch(lowerCmd) {
+				case "getmoonphase":
+					player.sendMessage("" + GetMoonPhase.getMoonPhase(world));
+					return true;
+				default:
+					player.sendMessage("Your command doesn't exist.");
+					return true;
+			}
+		}
+		
+		return true;
+	 }
+}
